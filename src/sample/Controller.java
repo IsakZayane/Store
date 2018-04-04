6620e0befe3ac0dce5bb5a7e376b041b59fa74cb
Joel Cohen <joel.cohen@hotmail.se>
2018-04-04 20:20:17 +0200

    joel andra försöket

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -8,6 +8,8 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable{
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("halli hallå gruppen!");
+        System.out.println("ett litet test /joel");
 
     }
 }
