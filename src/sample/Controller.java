1385f6cc9af4f30e9ba995d409f77ddcf8fc56b1
Isak Zayane <isak.zayane@hotmail.com>
2018-04-05 11:49:30 +0200

    Merge branch 'master' of C:\Users\Anon\Downloads\FInalversion\Store with conflicts.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,15 +1,4 @@
 package sample;
 
-import javafx.fxml.Initializable;
-
-import java.net.URL;
-import java.util.ResourceBundle;
-
-public class Controller implements Initializable{
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        System.out.println("halli hallå gruppen!");
-        System.out.println("ett litet test /joel");
-
-    }
+public class Controller {
 }
