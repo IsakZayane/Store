019812383c3f00a316b3f509cfedee81af00072d
Joel Cohen <joel.cohen@hotmail.se>
2018-04-10 11:13:15 +0200

    Testing gitignore

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello you");
+        primaryStage.setTitle("CatFood!");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
 
