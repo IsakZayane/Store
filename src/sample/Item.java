3a609538f0e51be2429828ac5c9052e67386cbf3
jonnyylay <38069559+jonnyylay@users.noreply.github.com>
2018-05-08 11:18:05 +0200

    obj

--- src/sample/Item.java
+++ src/sample/Item.java
@@ -4,11 +4,17 @@ public abstract class Item {
     private String name;
     private Double price;
     private Double weight;
+    private String ingredients;
+    private String origin;
+    private String detail;
 
     {
         this.name=name;
         this.price=price;
         this.weight=weight;
+        this.ingredients=ingredients;
+        this.origin=origin;
+        this.detail=detail;
     }
 
     public String getName() {
@@ -33,5 +39,29 @@ public abstract class Item {
 
     public void setWeight(Double weight) {
         this.weight = weight;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
     }
 }
